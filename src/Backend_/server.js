8eaf5c3e0c8e0c91a4c740ac698e31850cb23c54
Anuj mishra <captainanuj2004@gmail.com>
2024-12-11 02:49:56 +0530

    added gitignore

--- src/Backend_/server.js
+++ src/Backend_/server.js
@@ -2,27 +2,28 @@ import express from "express";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import cors from "cors";
+import { config } from "dotenv";
+config();
 const app = express();
 
-// Middleware
+
 app.use(cors());
 app.use(bodyParser.json());
 
-// Connect to MongoDB
-const mongoURI = "mongodb://localhost:27017/pass_Manager"; // Local MongoDB
+const mongoURI = process.env.Mongo_URI
 mongoose
   .connect(mongoURI)
   
-// Define Schema and Model
+
 const passwordSchema = new mongoose.Schema({
-  site: String,
-  username: String,
-  password: String,
+  site: { type: String, required: true },
+  username: { type: String, required: true },
+  password: { type: String, required: true },
 });
 
 const Password = mongoose.model("Password", passwordSchema);
 
-// API Endpoints
+
 app.get("/passwords", async (req, res) => {
   const passwords = await Password.find();
   res.json(passwords);
@@ -47,6 +48,5 @@ app.put("/passwords/:id", async (req, res) => {
   res.send("Password updated successfully");
 });
 
-// Start the Server
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
