c923093e389674f743296fc0a526753a3f24c220
Anuj mishra <captainanuj2004@gmail.com>
2025-07-23 23:09:36 +0530

    deploying changes

--- src/Backend_/server.js
+++ src/Backend_/server.js
@@ -7,7 +7,11 @@ config();
 const app = express();
 
 
-app.use(cors());
+app.use(cors({
+  origin: "https://passmanagerapp.netlify.app",  
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(bodyParser.json());
 
 const mongoURI = process.env.MONGO_URI
@@ -51,5 +55,5 @@ app.put("/passwords/:id", async (req, res) => {
   res.send("Password updated successfully");
 });
 
-const PORT = 5000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
